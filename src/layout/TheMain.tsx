90ca34a4baf835272c360f87834cc9a550d51764
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2024-10-28 22:07:12 -0400

    mater code

--- src/layout/TheMain.tsx
+++ src/layout/TheMain.tsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Todo from '#types/Todo';
 import ATodo from '#components/ATodo';
 import TheAddTodo from '#components/TheAddTodo';
-import useLocalStorage from '#composables/useLocalStorage';
 
 const TheMain = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [getItem, setItem] = useLocalStorage<Todo[]>();
-  const [loaded, setLoaded] = useState(false);
-
-  // besoin du loaded car todos, lors de sa création avec le useState, appelle le useEffet
-
-  useEffect(() => {
-    const item = getItem();
-    console.log('🚀 ~ useEffect ~ get todos:', item);
-    if (item) {
-      setTodos(item);
-    }
-    setLoaded(true);
-  }, []);
-
-  useEffect(() => {
-    console.log('🚀 ~ useEffect ~ save todos:', loaded);
-    if (loaded) setItem(todos);
-  }, [todos]);
 
   const handleAddTodo = (todo: Todo) => {
     setTodos([...todos, todo]);
